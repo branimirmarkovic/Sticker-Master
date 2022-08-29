51951928e53a4f9ec0f2966b4ec3455637aae823
Branimir Makovic <branimir.markovic@ezderm.com>
2022-08-29 19:24:08 +0200

    Initial Commit

---
+++ Sticker MasterUITests/Sticker_MasterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sticker_MasterUITestsLaunchTests.swift
+//  Sticker MasterUITests
+//
+//  Created by Branimir Markovic on 29.8.22..
+//
+
+import XCTest
+
+final class Sticker_MasterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
